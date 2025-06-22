e97926c79a0f3b2875a7c8e4789029842a75d179
Deniz Çakmakçı <dnzckmc@gmail.com>
2025-06-22 22:35:25 +0300

    Add assets

---
+++ lib/core/constants/assets.dart
@@ -0,0 +1,24 @@
+class Assets {
+  //IMAGES
+  static const String onboardGetStarted = 'assets/images/onboard1.png';
+  static const String onboardGetStartedBG =
+      'assets/images/onboard1_background.png';
+  static const String onboardPhoto = 'assets/images/onboard2.png';
+  static const String onboardInfo = 'assets/images/onboard3.png';
+  static const String onboardBrush = 'assets/images/onboard_brush.png';
+  static const String paywallBackground =
+      'assets/images/paywall_background.png';
+  static const String homePlants = 'assets/images/home_plants.png';
+
+  //ICONS
+  static const String logo = 'assets/icons/logo.svg';
+  static const String scanner = 'assets/icons/scanner.svg';
+  static const String speedometer = 'assets/icons/speedometer.svg';
+  static const String close = 'assets/icons/close.svg';
+  static const String message = 'assets/icons/message.svg';
+  static const String chevronRight = 'assets/icons/chevron_right.svg';
+  static const String search = 'assets/icons/search.svg';
+
+  //LOTIE
+  static const String plantSplash = 'assets/lottie/plant_splash.json';
+}
